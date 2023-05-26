b4ca3c11f25f883fddca014ed4121058bf9344cc
Shafin_ahmed <Ahmedshafin112233@gmail.com>
2023-05-26 14:36:10 +0600

    f

---
+++ spread_operator.js
@@ -0,0 +1,14 @@
+//normal ruls
+var number = [1, 2, 3];
+
+var new_nembers = [number[0], number[1],number[2], 2, 4, 5];
+
+console.log(new_nembers);
+
+//spread rules
+
+var number = [1, 2, 3];
+var new_a = [...number, 4, 5];
+
+console.log(new_a);
+
