6ee8072afe6470eda23129793ff96cd31ebd0911
Shafin_ahmed <Ahmedshafin112233@gmail.com>
2023-05-29 13:19:35 +0600

    today

---
+++ ternary.js
@@ -0,0 +1,12 @@
+var age = 20;
+
+var type = age > 30 ? "yes" : "no";
+
+console.log(type);
+
+//nested if
+var age2 = 16;
+var type_two =
+  age2 == 18 ? "adult" : age2 < 18 ? "child" : age2 > 18 ? "old" : "child";
+
+console.log(type_two);
